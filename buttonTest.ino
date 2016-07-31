6d4dc83d21824e79c24af5f30049971f96b3c7d4
Addeq <addeqcd@gmail.com>
2016-07-31 15:01:35 +0800

    Base File. Waiting confirmation to send data to vb

---
+++ buttonTest.ino
@@ -0,0 +1,29 @@
+
+String trigger  = "OK"; 
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(10,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int a = digitalRead(10);
+      if (Serial.available() > 0){
+      trigger = Serial.readString();
+      }
+  if (a == HIGH){
+    if (trigger == "OK"){
+    
+    Serial.println("INPUT");
+    digitalWrite(13,HIGH);
+    trigger = "NO";
+    }
+  }
+  else {
+    digitalWrite(13,LOW);
+  }
+  // delay(2000);
+
+   
+
+}
